f7360925d0128bd47231187b99d8ea1972b94142
Rafael Pierre <rafael.pierre@databricks.com>
2022-07-13 22:35:15 +0200

    camel case

--- telco_churn_mlops/jobs/model/entrypoint.py
+++ telco_churn_mlops/jobs/model/entrypoint.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+import mlflow
+>>>>>>> c21688d (camel case)
 from telco_churn_mlops.common import Job
 from telco_churn_mlops.pipelines.trainer import ModelTrainingPipeline
 
@@ -14,6 +18,10 @@ class TrainModelJob(Job):
             experiment_name = self.conf["experiment_name"],
             model_name = self.conf["model_name"]
         )
+<<<<<<< HEAD
+=======
+        mlflow.set_experiment()
+>>>>>>> c21688d (camel case)
         trainer.train()
 
         self.logger.info("training job finished!")
